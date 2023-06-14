6d48530db2e918a60547bd67e0a6553bb7c1b1f6
wzono <wzono.me@gmail.com>
2023-06-14 23:48:53 +0800

    fix: remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,10 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
     'next',
     'next/core-web-vitals',
   ],
   rules: {
-    'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
